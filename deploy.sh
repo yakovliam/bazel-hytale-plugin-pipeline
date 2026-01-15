be1d2d9da8edaf1bba89c77d0c780dfbc2f4a52e
Jacob Cohen <jacob@yakovliam.com>
2026-01-15 05:34:18 -0500

    chore: update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -24,3 +24,19 @@ export PIPELINE=dev
 
 # Build all targets
 bazel build //... --//build_flags:pipeline=$PIPELINE
+
+# Locate dist
+EXECROOT=$(bazel info execution_root)
+OUTPUT_REL=$(bazel cquery //plugin:dist --output=files | tail -n1)
+DIST_FILE="$EXECROOT/$OUTPUT_REL"
+
+if [ -n "$DIST_FILE" ]; then
+    echo "Moving $DIST_FILE to server mods..."
+    mkdir -p /home/server/mods
+    cp "$DIST_FILE" /home/server/mods/
+else
+    echo "Error: Could not locate distribution file."
+    exit 1
+fi
+
+echo "Deployed plugin to server."
